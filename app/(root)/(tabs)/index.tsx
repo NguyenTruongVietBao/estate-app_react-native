d5e74ddb5fb398cf39bef87c30a1e98b46178e55
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2024-12-23 11:41:36 +0700

    config nativewind

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="font-bold text-3xl">Hello</Text>
+      <Link href={"/sign-in"}>SignIn</Link>
+      <Link href={"/explore"}>Explore</Link>
+      <Link href={"/profile"}>Profile</Link>
+      <Link href={"/properties/1"}>Property details</Link>
+    </View>
+  );
+}
